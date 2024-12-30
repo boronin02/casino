6a548a6748b8637935b73e773683183570370771
Boronin Ivan <boronin02@yandex.ru>
2024-12-30 16:18:58 +0700

    added Wheel page

--- src/redux/balanceSlice.js
+++ src/redux/balanceSlice.js
@@ -1,5 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+export const fetchBalance = (token) => async (dispatch) => {
+    try {
+        const response = await fetch("http://localhost:8000/api/account/balance", {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch balance");
+        }
+
+        const data = await response.json();
+        dispatch(setBalance(data.user_balance));
+    } catch (error) {
+        console.error("Error fetching balance:", error);
+    }
+};
+
 const balanceSlice = createSlice({
     name: "balance",
     initialState: {
